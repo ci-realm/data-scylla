7db0ad3222a422c5490934235a4572ea26e46c2f
Richard Marko <srk@48.io>
2018-11-24 22:40:08 +0100

    drop redundant import

--- src/Scylla/Types/BuildStatus.hs
+++ src/Scylla/Types/BuildStatus.hs
@@ -3,8 +3,6 @@ module Scylla.Types.BuildStatus where
 
 import Data.Aeson.TH
 
-import Data.Time.LocalTime
-
 import Scylla.Types.Utils
 
 data BuildStatus = Success | Failure | Build | Queue | Clone | Checkout
